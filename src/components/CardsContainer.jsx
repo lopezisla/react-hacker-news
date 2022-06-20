349e6e8a0b42dc629990e622565cf2cc021b7d5d
Jorge Lopez Isla <jorgelopezisla@gmail.com>
2022-06-19 21:48:04 -0300

    feat: call api

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,11 +1,13 @@
-
+import Card from "./Card";
 import "./CardsContainer.css";
 
 const CardsContainer = () => {
   return (
     <>
       <div className="Container-Card">
-      
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
       </div>
     </>
   );
--- src/context/HackerNewsProvider.jsx
+++ src/context/HackerNewsProvider.jsx
@@ -8,6 +8,23 @@ const HackerNewsProvider = ({ children }) => {
   const [faves, setFaves] = useState(
     JSON.parse(localStorage.getItem("faves") ?? "[]")
   );
+
+  const checkPostsType = localStorage.getItem("postsType") || "all";
+  const [postsType, setPostsType] = useState(checkPostsType);
+
+  useEffect(() => {
+    if (postsType === "all") {
+      const queryAPI = async () => {
+        const url = `https://hn.algolia.com/api/v1/search_by_date?hitsPerPage=110`;
+        const { data } = await axios(url).catch((err) => {
+          console.error(err);
+        });
+        const filteredNews = filterNews(data.hits);
+        setNews(filteredNews);
+      };
+      queryAPI();
+    } 
+  }, [postsType]);
   
   const values = {
     news,
